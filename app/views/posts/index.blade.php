4dcf20b474b473305fb8ce2c4569383fff008895
pascalallen <thomaspascalallen@yahoo.com>
2016-01-11 14:38:39 -0600

    Styling posts index page.

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,24 +1,32 @@
 @extends('layouts.master')
 
+@section('top-script')
+	{{-- CUSTOM CSS --}}
+	<link rel="stylesheet" type="text/css" href="/css/posts-index.css">
+	{{-- CUSTOM FONT --}}
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+	{{-- FONT AWESOME --}}
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+@stop
+
 @section('content')
 
 <div class="container">
-	<h2>Blog Posts</h2>
-	<p>Check it out!</p>            
-	<table class="table table-condensed">
+	<div class="header">Bloggy Blog</div>
+	<div class="subheader">Blog Stuffz</div>
+	<hr>
+	<table class="table table-nonfluid center-table">
 		<thead>
 			<tr>
 				<th>Title</th>
 				<th>Body</th>
-				<th>Id</th>
 			</tr>
 		</thead>
 		<tbody>
 				@foreach ($posts as $post)
 					<tr>
-						<td><a href="{{{ action('PostController@show', $post->id) }}}">{{{ $post->title }}}</a></td>
+						<td><a href="{{{ action('PostController@show', $post->id) }}}" class="posts-title">{{{ $post->title }}}</a></td>
 						<td>{{{ $post->body }}}</td>
-						<td>{{{ $post->id }}}</td>
 					</tr>
 				@endforeach
 		</tbody>
