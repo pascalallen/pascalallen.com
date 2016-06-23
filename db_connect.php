346a33a1cf784dc2255edc00602fb3b01bb1d641
pascalallen <thomaspascalallen@yahoo.com>
2016-06-23 17:11:57 -0500

    Still modifying logins file. trying to get it to connect to deployed db.

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host='. $_ENV['DB_HOST'] .';dbname='. $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+$dbc = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME, DB_USER, DB_PASS);
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
