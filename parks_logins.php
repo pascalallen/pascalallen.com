2411622b9b18d7f472b289b4a9f75b38c4969e07
pascalallen <thomaspascalallen@yahoo.com>
2016-06-23 17:09:41 -0500

    Using env to config vanilla php dbc to work with laravel

--- parks_logins.php
+++ parks_logins.php
@@ -4,8 +4,8 @@
 // define('DB_USER', 'parks_user');
 // define('DB_PASS', 'ihave555');
 
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'blog_db');
-define('DB_USER', 'blog_user');
-define('DB_PASS', 'blog_pass');
+// define('DB_HOST', '127.0.0.1');
+// define('DB_NAME', 'blog_db');
+// define('DB_USER', 'blog_user');
+// define('DB_PASS', 'blog_pass');
 
