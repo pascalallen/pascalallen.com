51733d74ef300565015b87ce81cee1c473b0479f
pascalallen <thomaspascalallen@yahoo.com>
2016-06-26 21:55:52 -0500

    completed seeder for national parks table in db. made national parks model.

--- app/models/NationalPark.php
+++ app/models/NationalPark.php
@@ -2,12 +2,18 @@
 
 class NationalPark extends \Eloquent {
 
+	protected $table = 'national_parks';
+
 	// Add your validation rules here
 	public static $rules = [
-		// 'title' => 'required'
+		'name'    			=> 'required|min:1|max:240',
+	    'location'     		=> 'required|min:1|max:240',
+	    'date_established'  => 'required|date',
+	    'area_in_acres'	    => 'required|min:1|max:99999999999',
+	    'description'		=> 'required|min:1|max:240'
 	];
 
 	// Don't forget to fill this array
-	protected $fillable = [];
+	protected $fillable = array('name', 'location', 'date_established', 'area_in_acres', 'description');
 
 }
