3d52cb8190dd1b3ea1c29d6cc3820772c59d9fb4
Pascal <pascal.allen88@gmail.com>
2023-04-21 14:01:18 -0500

    add to index page

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,4 +1,5 @@
 const container = document.getElementById('root');
 if (container !== null) {
-    container.innerHTML = '<h1>Hello, World!</h1>';
+    container.innerHTML = '<h1>Hello, World!</h1>' +
+        '<p>My name is <strong>Pascal Allen</strong>, and I develop software.</p>';
 }
