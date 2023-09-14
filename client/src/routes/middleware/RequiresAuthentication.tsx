ccb324106c143484bccfd2ea2b544688d6e07f81
Pascal <pascal.allen88@gmail.com>
2023-09-14 05:17:51 -0500

    rm comments

--- client/src/routes/middleware/RequiresAuthentication.tsx
+++ client/src/routes/middleware/RequiresAuthentication.tsx
@@ -16,10 +16,6 @@ const RequiresAuthentication = observer((props: RequiresAuthenticationProps): Re
   const location = useLocation();
 
   if (!authService.isLoggedIn()) {
-    // Redirect them to the /login page, but save the current location they
-    // were trying to go to when they were redirected. This allows us to send
-    // them along to that page after they log in, which is a nicer user
-    // experience than dropping them off on the home page.
     return <Navigate to={Path.LOGIN} state={{ from: location }} replace />;
   }
 
--- client/src/routes/middleware/RequiresAuthorization.tsx
+++ client/src/routes/middleware/RequiresAuthorization.tsx
@@ -17,10 +17,6 @@ const RequiresAuthorization = observer((props: RequiresAuthorizationProps): Reac
   const location = useLocation();
 
   if (!authService.isLoggedIn()) {
-    // Redirect them to the /login page, but save the current location they
-    // were trying to go to when they were redirected. This allows us to send
-    // them along to that page after they log in, which is a nicer user
-    // experience than dropping them off on the home page.
     return <Navigate to={Path.LOGIN} state={{ from: location }} replace />;
   }
 
