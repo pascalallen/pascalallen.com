9c60881e8c6135868f7ee9b4426f836ba2686539
pascalallen <pascal.allen88@gmail.com>
2024-03-01 12:19:48 -0600

    rm superfluous pointer instances of event dispatcher and message bus, fix interfaces and types used by command bus and event dispatcher

--- internal/pascalallen/application/command_handler/mail.go
+++ internal/pascalallen/application/command_handler/mail.go
@@ -3,12 +3,13 @@ package command_handler
 import (
 	"fmt"
 	"github.com/pascalallen/pascalallen.com/internal/pascalallen/application/command"
+	"github.com/pascalallen/pascalallen.com/internal/pascalallen/infrastructure/messaging"
 	"log"
 )
 
 type SendWelcomeEmailHandler struct{}
 
-func (h SendWelcomeEmailHandler) Handle(cmd interface{}) error {
+func (h SendWelcomeEmailHandler) Handle(cmd messaging.Command) error {
 	c, ok := cmd.(*command.SendWelcomeEmail)
 	if !ok {
 		return fmt.Errorf("invalid command type passed to SendWelcomeEmailHandler: %v", cmd)
