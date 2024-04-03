5a8e6767611ba0a702ebaf748d13e6b8213d86b3
pascalallen <pascal.allen88@gmail.com>
2024-04-03 17:07:21 -0500

    fmt wire.go

--- cmd/pascalallen/wire.go
+++ cmd/pascalallen/wire.go
@@ -11,6 +11,16 @@ import (
 )
 
 func InitializeContainer() Container {
-	wire.Build(NewContainer, database.NewGormSession, repository.NewGormPermissionRepository, repository.NewGormRoleRepository, repository.NewGormUserRepository, database.NewDatabaseSeeder, messaging.NewRabbitMQConnection, messaging.NewRabbitMqCommandBus, messaging.NewRabbitMqEventDispatcher)
+	wire.Build(
+		NewContainer,
+		database.NewGormSession,
+		repository.NewGormPermissionRepository,
+		repository.NewGormRoleRepository,
+		repository.NewGormUserRepository,
+		database.NewDatabaseSeeder,
+		messaging.NewRabbitMQConnection,
+		messaging.NewRabbitMqCommandBus,
+		messaging.NewRabbitMqEventDispatcher,
+	)
 	return Container{}
 }
